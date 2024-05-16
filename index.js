e09266da8e190746055c915707877d74e20175f1
Pranav Aditya <148975644+Pranav92005@users.noreply.github.com>
2024-05-16 19:55:32 +0530

    uploaded

---
+++ index.js
@@ -0,0 +1,23 @@
+// const navbar = document.querySelector('.nav');
+// const links = document.querySelectorAll('.nav li');
+
+// links.forEach(link => {
+//     link.addEventListener('click', () => {
+//         navbar.classList.toggle('show-dropdown');
+//     });
+// });
+
+// const images = document.querySelectorAll('.scroll-container img');
+// let scrollPosition = 0;
+
+// function scrollImages() {
+//     scrollPosition -= 1; // Adjust the speed (negative value for leftward scroll)
+//     images.forEach(img => {
+//         img.style.transform = `translateX(${scrollPosition}px)`;
+//     });
+//     requestAnimationFrame(scrollImages);
+// }
+
+// scrollImages(); // Start the animation
+
+
